cb3f54909e6865a8dcd29f53a50bc0f217fcd791
DaffaAlwanMuktie <12200594@bsi.ac.id>
2021-09-28 13:24:12 +0700

    pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+class Contoh1 extends CI_Controller 
+{ 
+  public function index() 
+  { 
+      echo "<h1>Perkenalkan</h1>"; 
+      echo"Nama Saya Daffa Alwan Muktie 
+      Saya tinggal di daerah Bogor olah raga yang saya sukai adalah 
+      Bulutangkis";
+    } 
+}
